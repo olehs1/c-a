64cd64152d940346d38b0ddb18199d25eea73ef4
Oleh <oleg_smagnov@mail.ru>
2018-11-02 11:25:54 +0200

    fix styles

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 const appName = 'currency-app';
 
 app.use(express.static(__dirname + `/dist/${appName}`));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`));
-});
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`)));
 
 app.listen(process.env.PORT || 8080);
